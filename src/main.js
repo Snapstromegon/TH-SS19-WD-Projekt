92fbcc8d740bfcb249a4629be4b58cc07d7884fb
Raphael Höser <raphael@hoeser.info>
2019-09-08 12:33:21 +0200

    Add Serviceworker with offline support.

--- src/main.js
+++ src/main.js
@@ -8,3 +8,13 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app');
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js', { scope: '/' }).then(function(reg) {
+    // Registrierung erfolgreich
+    console.log('Registrierung erfolgreich. Scope ist ' + reg.scope);
+  }).catch(function(error) {
+    // Registrierung fehlgeschlagen
+    console.log('Registrierung fehlgeschlagen mit ' + error);
+  });
+};
